fdcc662ea9ca83cc3e113fec53c3944ff4420309
Zinoviev-Mkl <mykhailo.zinoviev@nure.ua>
2024-11-24 02:37:54 +0200

    second commit

--- lib/widgets/student_item.dart
+++ lib/widgets/student_item.dart
@@ -2,34 +2,38 @@ import 'package:flutter/material.dart';
 import 'package:zinoviev_kiuki_21_8/models/student.dart';
 
 class StudentItem extends StatelessWidget {
-  const StudentItem({super.key, required this.student});
+  const StudentItem({super.key, required this.student, required this.onSelectStudent});
 
   final Student student;
+  final void Function() onSelectStudent;
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: student.gender == Genders.male ? Colors.blue : Colors.pink,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16,
-          vertical: 14,
-        ),
-        child: Row(
-          children: [
-            Text(
-              '${student.firstName} ${student.lastName}',
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const Spacer(),
-            Row(
-              children: [
-                Icon(departmentIcons[student.department]),
-                const SizedBox(width: 8),
-                Text(student.grade.toString()),
-              ],
-            )
-          ],
+    return InkWell(
+      onTap: onSelectStudent, 
+      child: Card(
+        color: student.gender == Genders.male ? Colors.blue : Colors.pink,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 14,
+          ),
+          child: Row(
+            children: [
+              Text(
+                '${student.firstName} ${student.lastName}',
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const Spacer(),
+              Row(
+                children: [
+                  Icon(departmentIcons[student.department]),
+                  const SizedBox(width: 8),
+                  Text(student.grade.toString()),
+                ],
+              )
+            ],
+          ),
         ),
       ),
     );
